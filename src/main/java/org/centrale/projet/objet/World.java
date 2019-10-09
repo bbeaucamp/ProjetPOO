c286eec6321eb871426b4a17e33e84ce07597762
JingXIAO <xiaojing010203@gmail.com>
2019-10-09 14:08:40 +0200

    TP4_fini

--- src/main/java/org/centrale/projet/objet/World.java
+++ src/main/java/org/centrale/projet/objet/World.java
@@ -70,7 +70,6 @@ public class World {
     public World() {
 
         // Initialiser dans un tableau "ArrayList" 
-        // Définir la taille pour chaque ArrayList
         this.listArchers = new ArrayList<>();
         this.listGuerriers = new ArrayList<>();
         this.listMages = new ArrayList<>();
@@ -91,76 +90,78 @@ public class World {
      * aient tous une position différente.
      */
     public void creeMondeAlea() {
-        int maxNbPersonnages = 30; // Nombre max de protagonistes par classe
+        // Nombre max de protagonistes par classe
+        int maxNbPersonnages = 30;
         Random generateurAleatoire = new Random();
-        
+
         // Archers
-        int nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        int nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         ArrayList<Point2D> positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Archer a = new Archer();
+
             a.setPos(p);
             this.listArchers.add(a);
         }
-        
+
         // Guerriers
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Guerrier g = new Guerrier();
             g.setPos(p);
             this.listGuerriers.add(g);
         }
-        
+
         // Mages
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Mage m = new Mage();
             m.setPos(p);
             this.listMages.add(m);
         }
-        
+
         // Paysans
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Paysan paysan = new Paysan();
             paysan.setPos(p);
             this.listPaysans.add(paysan);
         }
-        
+
         // Loups
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Loup loup = new Loup();
             loup.setPos(p);
             this.listLoups.add(loup);
         }
-        
+
         // Lapins
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Lapin l = new Lapin();
             l.setPos(p);
             this.listLapins.add(l);
         }
-        
+
         // Soins
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Soin s = new Soin();
             s.setPos(p);
             this.listSoins.add(s);
         }
-        
+
         // Manas
-        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages); 
+        nbPersonnages = generateurAleatoire.nextInt(maxNbPersonnages);
         positions = this.generePositionsAleatoires(nbPersonnages);
-        for(Point2D p : positions){
+        for (Point2D p : positions) {
             Mana mana = new Mana();
             mana.setPos(p);
             this.listManas.add(mana);
@@ -178,34 +179,29 @@ public class World {
         // On ajoute un point à positions dès lors qu'il n'existe pas déjà
         for (int i = 1; i < nbPositions; i++) {
             newPos = new Point2D(generateurAleatoire.nextInt(this.tailleMonde), generateurAleatoire.nextInt(this.tailleMonde));
-            while (positions.contains(newPos)) {
+            while (positions.contains(newPos) && this.verfieDistanceMin(positions, newPos)) {
                 newPos = new Point2D(generateurAleatoire.nextInt(this.tailleMonde), generateurAleatoire.nextInt(this.tailleMonde));
             }
             positions.add(newPos);
-           
+
         }
 
         return positions;
     }
 
+    public boolean verfieDistanceMin(ArrayList<Point2D> positions, Point2D newPos) {
+        boolean res = true;
+        for (Point2D p : positions) {
+            if (p.distance(newPos) < 3) {
+                res = false;
+
+            }
+
+        }
+        return res;
+    }
+
     public void tourDeJeu() {
     }
 
-    /**
-     * Affichage textuel de tous les attributs de tous les créatures du monde.
-     */
-//    public void afficheWorld() {
-//
-//        System.out.println("Tous les personnages du monde :");
-//        this.grosBill.affiche();
-//        this.guillaumeT.affiche();
-//        this.merlin.affiche();
-//        this.peon.affiche();
-//        this.robin.affiche();
-//        System.out.println("Tous les monstres du monde :");
-//        this.bugs1.affiche();
-//        this.bugs2.affiche();
-//        this.wolfie.affiche();
-//    }
-    
 }
