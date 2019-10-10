9e4067589bd1ff0cb1265358973f7b873c69e940
CoderCyrus <cyrusbilibili@gmail.com>
2019-10-10 13:38:32 +0200

    abstract_ajoute

---
+++ src/main/java/org/centrale/projet/objet/NuageToxique.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+
+/**
+ *
+ * @author muruowang
+ */
+public class NuageToxique extends Objet implements Deplacable, Combattant {
+
+    // Override method depalcer() in class Deplaceble
+    @Override
+    public void deplacer() {
+    }
+
+  
+    
+    // Override method combattre(Creature c) in clasee Combattant
+
+    /**
+     *
+     * @param c
+     */
+      @Override
+      public void combattre(Creature c) {
+    }
+;
+}
